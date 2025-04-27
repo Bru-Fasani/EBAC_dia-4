85a3da52157294372b4b2c1f5d08bd9a5c4086fe
Bru-Fasani <brunafasani.arts@gmail.com>
2025-04-27 15:44:58 -0300

    Update EBAC.cy.js

--- cypress/e2e/EBAC.cy.js
+++ cypress/e2e/EBAC.cy.js
@@ -1,14 +1,13 @@
 /// <referece types="cypree"/>
 
 describe('teste de funcionalidades', () => {
+beforeEach(() => {
+  cy.visit('')
+});
+
   it('cadastro de campos obrigatótrio', () => {
-    cy.visit('http://127.0.0.1:8080/')
-    cy.get('#signup-firstname').type('teste')
-    cy.get('#signup-lastname').type('ebac')
-    cy.get('#signup-email').type('test@ebac.com')
-    cy.get('#signup-phone').type('110987743')
-    cy.get('#signup-password').type('Teste&2024')
-    cy.get('#signup-button').click()
+    var email = `teste${Date.now()}@qa.com`
+     cy.preechercadarstro('Teste', 'EBAC', email, '1265425373', 'Teste@ebac2025')
     cy.get('#signup-response').should('contain','Cadastro realizado com sucesso!')
   })
 })
